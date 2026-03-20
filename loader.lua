3261b48d261059ffe50c6cd2818926af6d5a97c1
shadeflux <shadefluxpl@gmail.com>
2026-03-20 19:41:15 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- Loader dla Quantum X MM2
-local url = "https://raw.githubusercontent.com/shadeflux/QuantumX-MM2/main/mm2.lua"
+local url = "https://raw.githubusercontent.com/shadeflux/QuantumX-MM2/refs/heads/main/mm2.lua?token=GHSAT0AAAAAADXFWBNSHHLXROEXOK742ICW2N5STXQ"
 local success, result = pcall(function()
     return game:HttpGet(url)
 end)
